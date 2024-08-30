040f40464d54a5865e4cf123ff6747945d300343
Martin Billinger-Finke <mbillingr@users.noreply.github.com>
2024-08-30 12:11:27 +0200

    replace remaining enum with class

--- index.ts
+++ index.ts
@@ -3,16 +3,77 @@ const TILE_SIZE = 30;
 const FPS = 30;
 const SLEEP = 1000 / FPS;
 
-enum RawTile {
-  AIR,
-  FLUX,
-  UNBREAKABLE,
-  PLAYER,
-  STONE, FALLING_STONE,
-  BOX, FALLING_BOX,
-  KEY1, LOCK1,
-  KEY2, LOCK2
+interface RawTileValue {
+  transform(): Tile;
 }
+class AirValue implements RawTileValue {
+    transform() { return new Air(); }
+}
+class FluxValue implements RawTileValue {
+    transform() { return new Flux(); }
+}
+class UnbreakableValue implements RawTileValue {
+    transform() { return new Unbreakable(); }
+}
+class PlayerValue implements RawTileValue {
+    transform() { return new PlayerTile(); }
+}
+class StoneValue implements RawTileValue {
+    transform() { return new Stone(new Resting()); }
+}
+class FallingStoneValue implements RawTileValue {
+    transform() { return new Stone(new Falling()); }
+}
+class BoxValue implements RawTileValue {
+    transform() { return new Box(new Resting()); }
+}
+class FallingBoxValue implements RawTileValue {
+    transform() { return new Box(new Falling()); }
+}
+class Key1Value implements RawTileValue {
+    transform() { return new Key(YELLOW_KEY); }
+}
+class Lock1Value implements RawTileValue {
+    transform() { return new TheLock(YELLOW_KEY); }
+}
+class Key2Value implements RawTileValue {
+    transform() { return new Key(BLUE_KEY); }
+}
+class Lock2Value implements RawTileValue {
+    transform() { return new TheLock(BLUE_KEY); }
+}
+
+class RawTile {
+  static readonly AIR = new RawTile(new AirValue());
+  static readonly FLUX = new RawTile(new FluxValue());
+  static readonly UNBREAKABLE = new RawTile(new UnbreakableValue());
+  static readonly PLAYER = new RawTile(new PlayerValue());
+  static readonly STONE = new RawTile(new StoneValue());
+  static readonly FALLING_STONE = new RawTile(new FallingStoneValue());
+  static readonly BOX = new RawTile(new BoxValue());
+  static readonly FALLING_BOX = new RawTile(new FallingBoxValue());
+  static readonly KEY1 = new RawTile(new Key1Value());
+  static readonly LOCK1 = new RawTile(new Lock1Value());
+  static readonly KEY2 = new RawTile(new Key2Value());
+  static readonly LOCK2 = new RawTile(new Lock2Value());
+
+  private constructor(private value: RawTileValue) {}
+
+  transform() {
+    return this.value.transform();
+  }
+}
+
+const RAW_TILES = [
+  RawTile.AIR,
+  RawTile.FLUX,
+  RawTile.UNBREAKABLE,
+  RawTile.PLAYER,
+  RawTile.STONE, RawTile.FALLING_STONE,
+  RawTile.BOX, RawTile.FALLING_BOX,
+  RawTile.KEY1, RawTile.LOCK1,
+  RawTile.KEY2, RawTile.LOCK2
+]
 
 interface Tile {
   isAir(): boolean;
@@ -255,8 +316,8 @@ class Player {
 
 class Map {
   private map: Tile[][];
-  constructor(rawMap: RawTile[][]) {
-    this.map = rawMap.map(row => row.map(transformTile));
+  constructor(rawMap: number[][]) {
+    this.map = rawMap.map(row => row.map(t => RAW_TILES[t].transform()));
   }
 
   update() {
@@ -321,7 +382,7 @@ class Map {
 
 let player = new Player();
 
-let rawMap: RawTile[][] = [
+let rawMap: number[][] = [
   [2, 2, 2, 2, 2, 2, 2, 2],
   [2, 3, 0, 1, 1, 2, 0, 2],
   [2, 4, 2, 6, 1, 2, 0, 2],
@@ -333,24 +394,6 @@ let rawMap: RawTile[][] = [
 function assertExhausted(x: never): never {
   throw new Error("Unexpected object: " + x);
 }
-function transformTile(tile: RawTile) {
-  switch (tile) {
-    case RawTile.AIR: return new Air();
-    case RawTile.PLAYER: return new PlayerTile();
-    case RawTile.UNBREAKABLE: return new Unbreakable();
-    case RawTile.STONE: return new Stone(new Resting());
-    case RawTile.FALLING_STONE: return new Stone(new Falling());
-    case RawTile.BOX: return new Box(new Resting());
-    case RawTile.FALLING_BOX: return new Box(new Falling());
-    case RawTile.FLUX: return new Flux();
-    case RawTile.KEY1: return new Key(YELLOW_KEY);
-    case RawTile.LOCK1: return new TheLock(YELLOW_KEY);
-    case RawTile.KEY2: return new Key(BLUE_KEY);
-    case RawTile.LOCK2: return new TheLock(BLUE_KEY);
-    default: assertExhausted(tile);
-  }
-}
-
 let inputs: Input[] = [];
 
 interface RemoveStrategy {
